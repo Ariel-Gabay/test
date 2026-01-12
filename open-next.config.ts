27fe9a53217810e7be3ca3856fdddf58177ed56f
Ariel Gabay <hag.lib.ada@gmail.com>
2026-01-12 12:40:32 +0200

    Hhyhy

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,13 +1,14 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 // import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
+// import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+// import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
 import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
 
 export default defineCloudflareConfig({
-  incrementalCache: withRegionalCache(r2IncrementalCache, {
-    mode: "long-lived",
-  }),
+  // incrementalCache: withRegionalCache(r2IncrementalCache, {
+  //   mode: "long-lived",
+  // }),
   tagCache: d1NextTagCache,
-  // incrementalCache: kvIncrementalCache,
+  incrementalCache: kvIncrementalCache,
 });
