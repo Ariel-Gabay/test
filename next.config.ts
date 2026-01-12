1ce1f0a0258b325d416235742e380beb7f55a966
Ariel Gabay <hag.lib.ada@gmail.com>
2026-01-12 12:52:10 +0200

    hnyn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        port: "",
+        pathname: "/images/**", // זה יכסה את כל התמונות בנתיב הזה
+      },
+    ],
+  },
 };
 
 export default nextConfig;
